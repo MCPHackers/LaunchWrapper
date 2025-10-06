0e826257832ff7edad10678d20c0fa5ce835f20d
Lassebq <lassebq.mine@gmail.com>
2025-10-07 00:41:44 +0300

    Bump to 1.2.1

--- src/main/java/org/mcphackers/launchwrapper/Launch.java
+++ src/main/java/org/mcphackers/launchwrapper/Launch.java
@@ -6,7 +6,7 @@ import org.mcphackers.launchwrapper.tweak.Tweak;
 
 public class Launch {
 
-	public static final String VERSION = "1.2";
+	public static final String VERSION = "1.2.1";
 	public static final Logger LOGGER = new Logger();
 
 	/**
